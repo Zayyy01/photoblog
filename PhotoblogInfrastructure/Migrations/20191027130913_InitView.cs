d617defea03c1df25a4240ef48237b9f1d1ca29e
Zaymus Gábor <gzaymus@gmail.com>
2019-10-29 15:33:12 +0100

    Add Core project

---
+++ PhotoblogInfrastructure/Migrations/20191027130913_InitView.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+
+namespace PhotoblogInfrastructure.Migrations
+{
+    public partial class InitView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        string script = @"CREATE VIEW [dbo].[ImageStoreView]
+AS
+SELECT [stream_id]
+      ,[file_stream]
+      ,[name]
+      ,CONVERT(VARCHAR(4000),[path_locator]) AS [path_locator]
+      ,CONVERT(VARCHAR(4000),[parent_path_locator]) AS [parent_path_locator]
+      ,[file_type]
+      ,[cached_file_size]
+      ,[creation_time]
+      ,[last_write_time]
+      ,[last_access_time]
+      ,[is_directory]
+      ,[is_offline]
+      ,[is_hidden]
+      ,[is_readonly]
+      ,[is_archive]
+      ,[is_system]
+      ,[is_temporary]
+  FROM [dbo].[ImageStore];";
+
+	        var config = new ConfigurationBuilder()
+		        .AddEnvironmentVariables()
+		        .Build();
+
+	        BlogDbContext ctx = new BlogDbContext(config.GetConnectionString("BlogDbConnectionString"));
+	        ctx.Database.ExecuteSqlRaw(script);
+
+
+
+            //migrationBuilder.DropPrimaryKey(
+            //    name: "PK_ImageStore",
+            //    table: "ImageStore");
+
+            //migrationBuilder.RenameTable(
+            //    name: "ImageStore",
+            //    newName: "ImageStoreView");
+
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "name",
+            //    table: "ImageStoreView",
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldType: "nvarchar(255)",
+            //    oldNullable: true);
+
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "file_type",
+            //    table: "ImageStoreView",
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldType: "nvarchar(255)",
+            //    oldNullable: true);
+
+            //migrationBuilder.AddPrimaryKey(
+            //    name: "PK_ImageStoreView",
+            //    table: "ImageStoreView",
+            //    column: "stream_id");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+	        string script = @"DROP VIEW dbo.ImageStoreView";
+
+	        var config = new ConfigurationBuilder()
+		        .AddEnvironmentVariables()
+		        .Build();
+
+	        BlogDbContext ctx = new BlogDbContext(config.GetConnectionString("BlogDbConnectionString"));
+	        ctx.Database.ExecuteSqlRaw(script);
+            //migrationBuilder.DropPrimaryKey(
+            //    name: "PK_ImageStoreView",
+            //    table: "ImageStoreView");
+
+            //migrationBuilder.RenameTable(
+            //    name: "ImageStoreView",
+            //    newName: "ImageStore");
+
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "name",
+            //    table: "ImageStore",
+            //    type: "nvarchar(255)",
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldNullable: true);
+
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "file_type",
+            //    table: "ImageStore",
+            //    type: "nvarchar(255)",
+            //    nullable: true,
+            //    oldClrType: typeof(string),
+            //    oldNullable: true);
+
+            //migrationBuilder.AddPrimaryKey(
+            //    name: "PK_ImageStore",
+            //    table: "ImageStore",
+            //    column: "stream_id");
+        }
+    }
+}
